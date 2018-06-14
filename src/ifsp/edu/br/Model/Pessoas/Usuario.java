0c6b61d7ce248221e0f7f14fcbfc64bf37608787
Mateus Inocentini <mateus.inocentini@gmail.com>
2018-06-14 17:37:39 -0300

    Coluna Usuario fixed

--- src/ifsp/edu/br/Model/Pessoas/Usuario.java
+++ src/ifsp/edu/br/Model/Pessoas/Usuario.java
@@ -10,13 +10,8 @@ public class Usuario extends Pessoa {
         super(id, nome, cpf, endereco, telefone, email);
     }
 
-//    @Override
-//    public String toString() {
-//        return "\nID: "+getId()+
-//                "\nNome: "+getNome()+
-//                "\nCPF: "+getCpf()+
-//                "\nEndereço: "+getEndereco()+
-//                "\nTelefone: "+getTelefone()+
-//                "\nEmail: "+getEmail();
-//    }
+    @Override
+    public String toString() {
+        return getNome();
+    }
 }
